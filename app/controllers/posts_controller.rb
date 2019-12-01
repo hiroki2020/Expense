5112edfe800bf64f93b3791228a986997333b4fe
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-01 15:52:35 +0900

    Create posts_controller.rb
    フラッシュメッセージを追加した。
    データを保存できるようにした。

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content],amount: params[:amount])
+    @post.save
+    redirect_to("/posts/index")
+    flash[:complete] = '記録しました。'
+  end
+end
