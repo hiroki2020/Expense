9bc29446a22b6658265457fcb6ce02c5faeb7d9d
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-01 21:22:43 +0900

    モーダル追加、ターボリンクの副作用の防止

--- app/assets/javascripts/_home.js
+++ app/assets/javascripts/_home.js
@@ -1,12 +1,31 @@
 $(function() {
-  $('#login-show').click(function() {
-    $('#login-modal').fadeIn();
+
+    document.addEventListener('turbolinks:load', function(){
+    
+    $('#btn').click(function() {
+      $('#login-modal').fadeIn();
+    });
+
+    $('.close-modal').click(function(){
+      $('#login-modal').fadeOut();
+    });
+
+    $('#hide').click(function() {
+      $('#amount-modal').fadeIn();
+    });
+
+    $('.close-modal').click(function(){
+      $('#amount-modal').fadeOut();
+    });
+
+    $('#hide').prop('disabled', false);
+    $('#login-show').prop('disabled', false);
+    
   });
 
-  $('.close-modal').click(function(){
-    $('#login-modal').fadeOut();
-  });
 });
+
+
 
 
 
