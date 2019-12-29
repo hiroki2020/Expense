5ca7b3a1b29deafa76cae5f81f341e43ad033cf3
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-30 08:10:32 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'posts/index'
-
+  post "user_progates/:id/update" => "user_progates#update"
+  get "user_progates/:id/edit" => "user_progates#edit"
+  post "user_progates/create" => "user_progates#create"
+  get "signup" => "user_progates#new"
+  get 'posts/index' 
+  get "user_progates/index" => "user_progates#index"
+  get "user_progates/:id" => "user_progates#show"
   root "home#top"
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
