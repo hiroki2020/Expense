a9f28bdc36a26280f3a82f875e582c995b21e992
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-31 11:17:12 +0900

    ログインアクションの記入

--- app/controllers/user_progates_controller.rb
+++ app/controllers/user_progates_controller.rb
@@ -12,7 +12,7 @@ class UserProgatesController < ApplicationController
   end
 
   def create
-    @user = UserProgate.new(name: params[:user_name], email: params[:user_email])
+    @user = UserProgate.new(name: params[:user_name], email: params[:user_email], password: params[:user_password])
     if @user.save
       redirect_to("/user_progates/#{@user.id}")
       flash[:notice] = "登録が完了しました！"
@@ -29,6 +29,7 @@ class UserProgatesController < ApplicationController
     @user = UserProgate.find_by(id: params[:id])
     @user.name = params[:user_name]
     @user.email = params[:user_email]
+    @user.password = params[:user_password]
     if @user.save
       redirect_to "/user_progates/#{@user.id}"
       flash[:notice] = "ユーザー情報を編集しました。"
@@ -36,4 +37,32 @@ class UserProgatesController < ApplicationController
       render("user_progates/edit")
     end
   end
+
+  def login_form
+  end
+
+  def login
+    @user = UserProgate.find_by(email: params[:email],
+                                password: params[:password])
+    if @user
+      flash[:notice] = "ログインしました"
+      session[:user_id] = @user.id
+      redirect_to("/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render("user_progates/login_form")
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトが完了しました"
+    redirect_to("/login")
+  end
+
+  
+
+  
 end
