25aa193bde665352e64f84814e2bb96c4d8dd1b6
hiroki2020 <n64.shibata.hiroki@gmail.com>
2020-01-01 11:02:49 +0900

    ユーザー周り

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_current_user
+
+  def set_current_user 
+    @current_user = UserProgate.find_by(id: session[:user_progate_id]) 
+  end
+
+  def authenticate_user
+    if @current_user == nil
+      flash[:notice] = "ログインまたは新規登録をしてください"
+      redirect_to("/login")
+    end
+  end
+
+  def forbit_login_user
+    if @current_user
+      flash[:notice] = "すでにログインしています"
+      redirect_to("/index")
+    end
+  end
+
 end
