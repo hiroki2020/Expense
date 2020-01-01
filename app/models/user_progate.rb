f6ee19e20962e0479e63356ef7aea54a178b62e3
hiroki2020 <n64.shibata.hiroki@gmail.com>
2020-01-01 14:46:08 +0900

    postsインスタンス関数
    上手く行かなかった

--- app/models/user_progate.rb
+++ app/models/user_progate.rb
@@ -2,4 +2,9 @@ class UserProgate < ApplicationRecord
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
   validates :password, {presence: true}
+
+  def posts
+    return Post.where(user_progate_id: self.id)
+  end
+  
 end
