e6f9695f6a05f4e786e9a2d3b508e956b318945c
hiroki2020 <n64.shibata.hiroki@gmail.com>
2020-01-01 14:45:31 +0900

    ポストモデルの編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   validates :content, {presence: true}
   validates :amount, {presence: true}  
+  validates :user_progate_id, {presence: true}
+
+  def user_progate
+    return UserProgate.find_by(id: self.user_progate_id)
+  end
+
 end
