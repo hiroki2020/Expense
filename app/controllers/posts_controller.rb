82378197afe72b19c0309213744e4ca0c273454f
hiroki2020 <n64.shibata.hiroki@gmail.com>
2020-01-01 11:03:57 +0900

    投稿　コントローラー　調整

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,19 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user
   def index
     @posts = Post.all.order("created_at DESC")
   end
 
   def new
+
   end
 
   def create
-    @post = Post.new(content: params[:content],amount: params[:amount])
+    @post = Post.new(
+      content: params[:content],
+      amount: params[:amount],
+      user_progate_id: @current_user.id
+    )
     @post.save
     redirect_to("/posts/index")
     flash[:complete] = '記録しました。'
@@ -24,6 +30,12 @@ class PostsController < ApplicationController
   def destroy
     post = Post.all.order("created_at DESC").find(params[:id])
     post.destroy
+    redirect_to("/posts/index")
+    flash[:notice] = "削除が完了しました。"
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
   end
 
 end
