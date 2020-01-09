e0c1f1be377807210c3d7bf8790937a727fe94b9
hiroki2020 <n64.shibata.hiroki@gmail.com>
2020-01-09 19:56:34 +0900

    Update schema.rb
    スキーマファイル
    rails db:migrateの結果が反映されたもの

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191231072727) do
+ActiveRecord::Schema.define(version: 20200109101449) do
 
   create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "namespace"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20191231072727) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "password"
+    t.string   "image_name"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
